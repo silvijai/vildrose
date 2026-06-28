680302fb6f2588646f0cafdca155e0e10cf29d6d
silvijai <silvijai@users.noreply.github.com>
2026-06-28 19:09:04 +0000

    deploy: 1380fecae78d4bbcb0a3901420f3de7c4a480161

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cabf99df.js',
+                path_to_root + 'searchindex-1cf0bc99.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
