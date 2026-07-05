cfb0a12abba083d4b949c3a9e456994a3d88af91
silvijai <silvijai@users.noreply.github.com>
2026-07-05 19:50:31 +0000

    deploy: 85a0edd54a51e0947f1f43a0c1f01f1db3ce76c6

--- api/src-files.js
+++ api/src-files.js
@@ -1,2 +1,2 @@
-createSrcSidebar('[["vildrose_core",["",[],["lib.rs","trit.rs","word.rs"]]]]');
-//{"start":19,"fragment_lengths":[56]}
+createSrcSidebar('[["vildrose_core",["",[],["lib.rs","ops.rs","trit.rs","word.rs"]]]]');
+//{"start":19,"fragment_lengths":[65]}
--- api/trait.impl/core/convert/trait.TryFrom.js
+++ api/trait.impl/core/convert/trait.TryFrom.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["vildrose_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.96.1/std/primitive.i8.html\">i8</a>&gt; for <a class=\"enum\" href=\"vildrose_core/trit/enum.Trit.html\" title=\"enum vildrose_core::trit::Trit\">Trit</a>",0]]]]);
+    const implementors = Object.fromEntries([["vildrose_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.96.1/std/primitive.i8.html\">i8</a>&gt; for <a class=\"enum\" href=\"vildrose_core/trit/enum.Trit.html\" title=\"enum vildrose_core::trit::Trit\">Trit</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.96.1/std/primitive.i16.html\">i16</a>&gt; for <a class=\"struct\" href=\"vildrose_core/word/struct.Tryte.html\" title=\"struct vildrose_core::word::Tryte\">Tryte</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.96.1/std/primitive.i64.html\">i64</a>&gt; for <a class=\"struct\" href=\"vildrose_core/word/struct.Word27.html\" title=\"struct vildrose_core::word::Word27\">Word27</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.96.1/std/primitive.i128.html\">i128</a>&gt; for <a class=\"struct\" href=\"vildrose_core/word/struct.Word54.html\" title=\"struct vildrose_core::word::Word54\">Word54</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[390]}
+//{"start":59,"fragment_lengths":[1544]}
